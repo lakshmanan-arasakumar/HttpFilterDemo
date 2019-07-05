0cf0a7738225a4d6f1e459fcb6e95a4707696d76
LARASAKU <larasaku@ford.com>
2019-07-05 15:28:16 -0400

    Enabling http filter using annotation

--- src/main/java/com/lakshmanan/arasakumar/HttpFilterDemo/HttpFilterDemoApplication.java
+++ src/main/java/com/lakshmanan/arasakumar/HttpFilterDemo/HttpFilterDemoApplication.java
@@ -1,9 +1,11 @@
 package com.lakshmanan.arasakumar.HttpFilterDemo;
 
+import configuration.EnableHttpFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableHttpFilter
 public class HttpFilterDemoApplication {
 
 	public static void main(String[] args) {
